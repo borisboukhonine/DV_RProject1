2d6342c8e7c53cfaa2efd307ee21daa605df16ed
julietteseive <julietteseive@gmail.com>
2015-02-04 23:55:46 -0600

    final stylistic changes

--- 02 Visualizations/Duplication.R
+++ 02 Visualizations/Duplication.R
@@ -9,7 +9,3 @@ p1 + facet_wrap(~CUSTOMER_STATE)
 p2 <- ggplot(df_all, aes(x=as.Date(ORDER_DATE, "%Y-%m-%d"), y=as.Date(SHIPPED_DATE, "%Y-%m-%d")))+geom_point(aes(color=as.factor(ARTIST)))
 
 p2 + facet_wrap(~CUSTOMER_STATE)
-
-p
-p1
-p2
--- 02 Visualizations/Int_Plots.R
+++ 02 Visualizations/Int_Plots.R
@@ -1,8 +1,8 @@
 interesting1 <- ggplot(data=df_customers)+geom_histogram(aes(x=CUSTOMER_STATE, fill=CUSTOMER_STATE))
-interesting2 <- ggplot(df_orders, aes(x=as.Date(ORDER_DATE, "%Y-%m-%d"), y=as.Date(SHIPPED_DATE, "%Y-%m-%d")))+geom_point()
-interesting3 <- ggplot(data=df_order_details)+geom_histogram(aes(x=ITEM_ID))
-interesting4 <- ggplot(df_items, aes(x=ARTIST, y=UNIT_PRICE, color=TITLE))+geom_point()
 interesting1
+interesting2 <- ggplot(df_orders, aes(x=as.Date(ORDER_DATE, "%Y-%m-%d"), y=as.Date(SHIPPED_DATE, "%Y-%m-%d")))+geom_point()
 interesting2
+interesting3 <- ggplot(data=df_order_details)+geom_histogram(aes(x=ITEM_ID))
 interesting3
+interesting4 <- ggplot(df_items, aes(x=ARTIST, y=UNIT_PRICE, color=TITLE))+geom_point()
 interesting4
